2aae27926f65916984200344d35570fdd42e702d
jinieeee <lje911@gmail.com>
2023-05-06 21:46:44 +0900

    [feat] test 코드 추가

--- src/test/java/com/example/tdd/TddApplicationTests.java
+++ src/test/java/com/example/tdd/TddApplicationTests.java
@@ -3,11 +3,27 @@ package com.example.tdd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TddApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testMultipleication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+	}
+}
+
+class Dollar {
+	int amount = 10;
+
+	Dollar(int amount) {
+		this.amount = amount;
 	}
 
+	void times(int multiplier) {
+		amount *= multiplier;
+	}
 }
